7103298ca77f0690f7f2c74c905bf43734a422d7
Cesar <kliko1989@gmail.com>
2021-03-15 04:23:06 -0600

    sqwswqw

--- sale_order_compatibles/models/models.py
+++ sale_order_compatibles/models/models.py
@@ -121,6 +121,8 @@ class sale_update(models.Model):
         ('entregado', 'Entregado'),
         ('cancel', 'Cancelled'),
         ], string='Status', readonly=True, copy=False, index=True, track_visibility='onchange', track_sequence=3, default='draft')
+	x_studio_tipo_de_solicitud=fields.Selection([["Cambio","Cambio"],["Arrendamiento","Arrendamiento"],["Venta","Venta"],["Backup","Backup"],["Demostración","Demostración"],["Retiro","Retiro"],["Préstamo","Préstamo"]])
+	
 	def Reporte(self):
 	    fecha=datetime.datetime.now().date()
 	    sa=self.search([['x_studio_tipo_de_solicitud','in',('Demostración','Préstamo')],['state','in',('sale','assign')]])
